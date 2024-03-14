50e3731aca2f3e6f6ebae618a4331a203da29357
qumi <bahz1997@gmail.com>
2024-03-15 01:33:16 +0400

    Решено ДЗ

---
+++ app_02.py
@@ -0,0 +1,35 @@
+"""
+� Напишите программу на Python, которая будет находить
+сумму элементов массива из 1000000 целых чисел.
+� Пример массива: arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ...]
+� Массив должен быть заполнен случайными целыми числами
+от 1 до 100.
+� При решении задачи нужно использовать многопоточность,
+многопроцессорность и асинхронность.
+� В каждом решении нужно вывести время выполнения
+вычислений.
+"""
+
+import multiprocessing
+import time
+import random
+
+
+def worker(arr):
+    print(sum(arr))
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    multiprocess = []
+    arr = []
+    for _ in range(0, 1000000):
+        arr.append(random.randint(0, 100))
+
+    t = multiprocessing.Process(target=worker, args=(arr,))
+    multiprocess.append(t)
+    t.start()
+
+    for t in multiprocess:
+        t.join()
+    print(f'Время выполнения {time.time() - start_time:.2f} сек')
